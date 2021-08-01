642a98f0a579e09d7273e4ffeb696fa13bed1dda
Aerthone <aekapitsa@gmail.com>
2021-08-01 12:33:57 +0300

    addition

---
+++ HomeTask1(v2)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeTask1_v2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! And hold my beer!");
+        }
+    }
+}
